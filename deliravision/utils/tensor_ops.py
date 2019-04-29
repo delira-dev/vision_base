0ff0f90924bead345c0ced1b3e0f3b183a9b7834
Justus Schock <12886177+justusschock@users.noreply.github.com>
2019-04-29 20:44:25 +0200

    Update tensor_ops.py

--- deliravision/utils/tensor_ops.py
+++ deliravision/utils/tensor_ops.py
@@ -1,9 +1,18 @@
-import torch
+from delira import get_backends
+import numpy as np
 
-def make_onehot(labels, n_classes):
-    labels = labels.view(-1, 1).long()
+def make_onehot_npy(labels, n_classes):
+    labels = labels.reshape(-1).astype(np.uint8)
+    return np.eye(n_classes)[labels]
 
-    labels_onehot = torch.zeros(labels.size(0), n_classes,
-                                device=labels.device, dtype=torch.float)
-    labels_onehot.scatter_(1, labels, 1)
-    return labels_onehot
+if "TORCH" in get_backends():
+
+    import torch
+
+    def make_onehot_torch(labels, n_classes):
+        labels = labels.view(-1, 1).long()
+
+        labels_onehot = torch.zeros(labels.size(0), n_classes,
+                                    device=labels.device, dtype=torch.float)
+        labels_onehot.scatter_(1, labels, 1)
+        return labels_onehot
