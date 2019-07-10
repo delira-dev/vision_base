48651c792adc17a7f231ccf3126d0f3a00388844
Justus Schock <justus.schock@rwth-aachen.de>
2019-07-10 09:30:53 +0200

    Add MUNIT

--- deliravision/losses.py
+++ deliravision/losses.py
@@ -1,6 +1,32 @@
 import torch
 
 
+class AdversarialLoss(torch.nn.Module):
+    def __init__(self, loss_fn=torch.nn.BCELoss(), same_size=False):
+        super().__init__()
+        self._loss_fn = loss_fn
+        self._same_size = same_size
+
+    def forward(self, pred: torch.Tensor, target: bool):
+        if self._same_size:
+            gt = torch.ones_like(pred)
+        else:
+            gt = torch.ones(pred.size(0), 1, device=pred.device,
+                            dtype=pred.dtype)
+
+        gt = gt * int(target)
+
+        return self._loss_fn(pred, gt)
+
+
+class IterativeDiscriminatorLoss(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, preds: torch.Tensor, gt):
+        return sum([torch.mean((out - gt) ** 2) for out in preds])
+
+
 class GradientPenalty(torch.nn.Module):
     """
     A module to compute the gradient penalty
