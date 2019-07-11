5e224decc63db1da954c3341091c2b73e643530c
Justus Schock <justus.schock@rwth-aachen.de>
2019-07-11 13:34:26 +0200

    Add BounadrySeeking Loss

--- deliravision/losses.py
+++ deliravision/losses.py
@@ -242,3 +242,42 @@ class BELoss(torch.nn.Module):
             the loss value
         """
         return (pred - label).abs().mean()
+
+
+class BoundarySeekingLoss(torch.nn.Module):
+    """
+    Boundary Seeking Loss
+
+    References
+    ----------
+    https://wiseodd.github.io/techblog/2017/03/07/boundary-seeking-gan/
+
+    """
+    def __init__(self, weight=0.5):
+        """
+
+        Parameters
+        ----------
+        weight : float
+            weighting factor
+        """
+        super().__init__()
+
+        self._weight = weight
+
+    def forward(self, pred):
+        """
+        Calculates the actual loss
+        Parameters
+        ----------
+        pred : :class:`torch.Tensor`
+            the prediction (typically obtained by the discriminator)
+
+        Returns
+        -------
+        :class:`torch.Tensor`
+            the loss value
+
+        """
+        return self._weight * torch.mean((torch.log(pred) -
+                                          torch.log(1 - pred)) ** 2)
