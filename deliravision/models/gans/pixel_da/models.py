8e8eb87099295dffe0b567726905ff43c03a20e8
Justus Schock <justus.schock@rwth-aachen.de>
2019-07-16 09:05:11 +0200

    Add docstrings

--- deliravision/models/gans/pixel_da/models.py
+++ deliravision/models/gans/pixel_da/models.py
@@ -2,7 +2,17 @@ import torch
 
 
 class ResidualBlock(torch.nn.Module):
+    """
+    A single Residual Block
+    """
     def __init__(self, num_filts):
+        """
+
+        Parameters
+        ----------
+        num_filts : int
+            number of input and output channels/filters
+        """
         super().__init__()
 
         self.block = torch.nn.Sequential(
@@ -14,12 +24,44 @@ class ResidualBlock(torch.nn.Module):
         )
 
     def forward(self, x):
+        """
+        Computes the residual output for a single batch
+
+        Parameters
+        ----------
+        x : :class:`torch.Tensor`
+            the image batch
+
+        Returns
+        -------
+        :class:`torch.Tensor`
+            the image batch
+
+        """
         return x + self.block(x)
 
 
 class Generator(torch.nn.Module):
+    """
+    The generator topology
+    """
     def __init__(self, latent_dim, num_channels, img_size, n_residual_blocks,
                  num_filts=64):
+        """
+
+        Parameters
+        ----------
+        latent_dim : int
+            size of the latent dimension
+        num_channels : int
+            number of image channels to generate
+        img_size : int
+            number of pixels per side of the image
+        n_residual_blocks : int
+            number of residual blocks inside the generator
+        num_filts : int
+            number of filters inside each of the blocks
+        """
         super().__init__()
 
         # Fully-connected layer which constructs image channel shaped output from noise
@@ -38,6 +80,22 @@ class Generator(torch.nn.Module):
             torch.nn.Conv2d(64, num_channels, 3, 1, 1), torch.nn.Tanh())
 
     def forward(self, img, z):
+        """
+        Feeds a set of batches through the network
+
+        Parameters
+        ----------
+        img : :class:`torch.Tensor`
+            the image tensor
+        z : :class:`torch.Tensor`
+            the noise tensor
+
+        Returns
+        -------
+        :class:`torch.Tensor`
+            batch of generated images
+
+        """
         gen_input = torch.cat((img, self.fc(z).view(*img.shape)), 1)
         out = self.l1(gen_input)
         out = self.resblocks(out)
@@ -47,7 +105,17 @@ class Generator(torch.nn.Module):
 
 
 class Discriminator(torch.nn.Module):
+    """
+    A discriminator network
+    """
     def __init__(self, num_channels):
+        """
+
+        Parameters
+        ----------
+        num_channels : int
+            number of channels per input image
+        """
         super().__init__()
 
         def block(in_features, out_features, normalization=True):
@@ -68,13 +136,41 @@ class Discriminator(torch.nn.Module):
         )
 
     def forward(self, img):
+        """
+        Feeds a single batch through the network
+
+        Parameters
+        ----------
+        img : :class:`torch.Tensor`
+            a single image batch
+
+        Returns
+        -------
+        :class:`torch.Tensor`
+            the resulting validity batch
+
+        """
         validity = self.model(img)
 
         return validity
 
 
 class Classifier(torch.nn.Module):
+    """
+    Classifier Network
+    """
     def __init__(self, num_channels, img_size, n_classes):
+        """
+
+        Parameters
+        ----------
+        num_channels : int
+            number of image channels
+        img_size : int
+            number of pixels per side
+        n_classes : int
+            number of classes
+        """
         super().__init__()
 
         def block(in_features, out_features, normalization=True):
@@ -99,6 +195,20 @@ class Classifier(torch.nn.Module):
             torch.nn.Softmax())
 
     def forward(self, img):
+        """
+        Feeds a single batch through the network
+
+        Parameters
+        ----------
+        img : :class:`torch.Tensor`
+            the image batch
+
+        Returns
+        -------
+        :class:`torch.Tensor`
+            the resulting label batch
+
+        """
         feature_repr = self.model(img)
         feature_repr = feature_repr.view(feature_repr.size(0), -1)
         label = self.output_layer(feature_repr)
