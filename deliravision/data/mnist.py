97a2c78cb4f621c791f583fd906dceabcddc0342
Justus Schock <justus.schock@rwth-aachen.de>
2019-07-18 10:34:39 +0200

    Add FashionMNIST, KMNIST and EMNIST

--- deliravision/data/mnist.py
+++ deliravision/data/mnist.py
@@ -4,6 +4,7 @@ import gzip
 import os
 import numpy as np
 from PIL import Image
+import zipfile
 
 
 class MNIST(ImageFolder, Downloadable):
@@ -36,7 +37,7 @@ class MNIST(ImageFolder, Downloadable):
         remove : bool
             whether to remove the downlaoded data after processing it
         """
-        root = os.path.join(root, "MNIST")
+        root = os.path.join(root, self.name)
 
         if train:
             root = os.path.join(root, "train")
@@ -77,6 +78,26 @@ class MNIST(ImageFolder, Downloadable):
         # check if images and labels have been loaded
         assert images is not None and labels is not None
 
+        self._to_image_folder(images, labels, prep_path)
+
+    @staticmethod
+    def _to_image_folder(images, labels, prep_path):
+        """
+        Helper Function, which writes the given images and labels to the given
+        path in a way, they can be read by the
+        :class:`deliravision.data.base_datasets.ImageFolder` class
+
+        Parameters
+        ----------
+        images : :class:`numpy.ndarray`
+            the array containing the images
+        labels : :class:`numpy.ndarray`
+            the array containing the labels
+        prep_path : str
+            the path which will contain the preprocessed data after
+            dumping the images
+
+        """
         # counter for each class
         label_idxs = {}
         for img, label in zip(images, labels):
@@ -192,3 +213,254 @@ class MNIST(ImageFolder, Downloadable):
             "http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz":
                 "t10k-labels-idx1-ubyte.gz"
         }
+
+    @property
+    def name(self):
+        """
+        Property returning the Datasets name to make the class reusable
+
+        Returns
+        -------
+        str
+            the name
+
+        """
+        return "MNIST"
+
+
+class FashionMNIST(MNIST):
+    """
+    The Fashion-MNIST Dataset
+
+    See Also
+    --------
+    :class:`deliravision.data.base_datasets.ImageFolder`
+        the Image Folder, this class is implemented upon.
+    :class:`deliravision.data.mnist.MNIST`
+        the original MNIST dataset
+    :class:`deliravision.mnist.KMNIST`
+        the Kuzushiji-MNIST dataset
+    :class:`deliravision.mnist.MNIST`
+        the extended MNIST dataset
+
+    References
+    ----------
+    https://github.com/zalandoresearch/fashion-mnist
+
+    """
+    @property
+    def name(self):
+        """
+        Property returning the Datasets name
+
+        Returns
+        -------
+        str
+            the name
+
+        """
+        return "FashionMNIST"
+
+    @property
+    def urls(self):
+        """
+        Property returning the urls of the current mode
+
+        Returns
+        -------
+        dict
+            dictionary containing either the train or test urls
+
+        """
+        if self.train:
+            return {
+                "http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/"
+                "train-images-idx3-ubyte.gz":
+                    "train-images-idx3-ubyte.gz",
+                "http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/"
+                "train-labels-idx1-ubyte.gz":
+                    "train-labels-idx1-ubyte.gz"
+            }
+
+        return {
+            "http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/"
+            "t10k-images-idx3-ubyte.gz":
+                "t10k-images-idx3-ubyte.gz",
+            "http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/"
+            "t10k-labels-idx1-ubyte.gz":
+                "t10k-labels-idx1-ubyte.gz"
+        }
+
+
+class KMNIST(MNIST):
+    """
+    The Kuzushiji-MNIST Dataset
+
+    See Also
+    --------
+    :class:`deliravision.data.base_datasets.ImageFolder`
+        the Image Folder, this class is implemented upon.
+    :class:`deliravision.data.mnist.MNIST`
+        the original MNIST dataset
+    :class:`deliravision.mnist.FashionMNIST`
+        the FashionMNIST dataset
+    :class:`deliravision.mnist.MNIST`
+        the extended MNIST dataset
+
+    References
+    ----------
+    https://github.com/rois-codh/kmnist
+
+    """
+
+    @property
+    def name(self):
+        """
+        Property returning the Datasets name
+
+        Returns
+        -------
+        str
+            the name
+
+        """
+        return "KMNIST"
+
+    @property
+    def urls(self):
+        """
+        Property returning the urls of the current mode
+
+        Returns
+        -------
+        dict
+            dictionary containing either the train or test urls
+
+        """
+        if self.train:
+            return {
+                "http://codh.rois.ac.jp/kmnist/dataset/kmnist/"
+                "train-images-idx3-ubyte.gz":
+                    "train-images-idx3-ubyte.gz",
+                "http://codh.rois.ac.jp/kmnist/dataset/kmnist/"
+                "train-labels-idx1-ubyte.gz":
+                    "train-labels-idx1-ubyte.gz"
+            }
+
+        return {
+            "http://codh.rois.ac.jp/kmnist/dataset/kmnist/"
+            "t10k-images-idx3-ubyte.gz":
+                "t10k-images-idx3-ubyte.gz",
+            "http://codh.rois.ac.jp/kmnist/dataset/kmnist/"
+            "t10k-labels-idx1-ubyte.gz":
+                "t10k-labels-idx1-ubyte.gz"
+        }
+
+
+class EMNIST(MNIST):
+    """
+    The Extended-MNIST Dataset
+
+    See Also
+    --------
+    :class:`deliravision.data.base_datasets.ImageFolder`
+        the Image Folder, this class is implemented upon.
+    :class:`deliravision.data.mnist.MNIST`
+        the original MNIST dataset
+    :class:`deliravision.mnist.FashionMNIST`
+        the FashionMNIST dataset
+    :class:`deliravision.mnist.KMNIST`
+        the Kuzushiji-MNIST dataset
+
+    References
+    ----------
+    https://www.westernsydney.edu.au/bens/home/reproducible_research/emnist
+
+    """
+    def __init__(self, root="/tmp", train=True, split="balanced",
+                 download=True, remove=False):
+        assert split in ('balanced', 'byclass', 'bymerge', 'digits', 'letters',
+                         'mnist')
+
+        self.split = split
+        self.train = train
+        root = os.path.join(root, self.name)
+
+        Downloadable.__init__(self, path=root, download=download,
+                              remove=remove)
+        ImageFolder.__init__(
+            self, os.path.join(root, split, "train" if train else "test"))
+
+    @property
+    def name(self):
+        """
+        Property returning the Datasets name
+
+        Returns
+        -------
+        str
+            the name
+
+        """
+        return "EMNIST"
+
+    @property
+    def urls(self):
+        """
+        Property returning the urls of the current mode
+
+        Returns
+        -------
+        dict
+            dictionary containing either the train or test urls
+
+        """
+        return {"https://cloudstor.aarnet.edu.au/plus/index.php/s/"
+                "54h3OuGJhFLwAlQ/download": "emnist_full.zip"}
+
+    def preprocess_data(self, download_path, prep_path):
+        """
+        Function to preprocess the downloaded data
+
+        Parameters
+        ----------
+        download_path : str
+            the path containing the downloaded data
+        prep_path : str
+            the path the preprocessed data should be stored in
+
+        """
+        fname = list(self.urls.values())[0]
+        with zipfile.ZipFile(os.path.join(download_path, fname)) as f:
+            f.extractall(download_path)
+
+        download_path = os.path.join(download_path, "gzip")
+
+        split_files = {k: os.path.join(download_path, v)
+                       for k, v in self._split_files.items()}
+
+        images = self._read_images_from_binary_gzip(split_files["images"])
+        labels = self._read_labels_from_binary_gzip(split_files["labels"])
+
+        train_str = "train" if self.train else "test"
+        self._to_image_folder(images, labels, os.path.join(prep_path,
+                                                           self.split,
+                                                           train_str))
+
+    @property
+    def _split_files(self):
+        """
+        Property returning the files in the archive for the current split and
+        train mode
+
+        Returns
+        -------
+        dict
+            dictionary containing the filenames for the images and labels file
+
+        """
+        train_str = "train" if self.train else "test"
+        return {"images": "emnist-%s-%s_images-idx3-ubtyte.gz"
+                          % (self.split, train_str),
+                "labels": "emnist-%s-%s-labels-idx1-ubyte.gz"
+                          % (self.split, train_str)}
